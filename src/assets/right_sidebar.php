49cce470c8cedeb335276fedcc98c0c141f156e3
RubenWinkler <r.winkler1412@gmail.com>
2017-03-08 22:03:35 +0100

    Layout realitätsnäher gemacht

--- src/assets/right_sidebar.php
+++ src/assets/right_sidebar.php
@@ -1,17 +1,31 @@
-<div class="right_sidebar_col col-lg-2 col-md-4 col-sm-4 hidden-xs">
-  <div class="login">
-    <div class="input-group">
-      <input type="text" class="form-control login_username" placeholder="Benutzername" aria-describedby="basic-addon2">
-      <input type="password" class="form-control login_password" placeholder="Passwort" aria-describedby="basic-addon2">
+<div class="col-lg-2 col-md-4 col-sm-4 hidden-xs" id="right_sidebar_col">
+  <div id="right_sidebar">
+    <div class="form-group" id="login">
+      <form>
+        <label for="login_username" class="sr-only">Benutzername:</label>
+          <input type="text" class="form-control" id="login_username" placeholder="Benutzername">
+        <label for="login_password" class="sr-only">Passwort:</label>
+          <input type="password" class="form-control" id="login_password" placeholder="Passwort">
+        <div class="checkbox" id="remember_me_checkbox">
+          <label><input type="checkbox">Angemeldet bleiben</label>
+        </div>
+        <div id="login_or_register">
+          <button type="submit" class="btn btn-danger">Anmelden</button>
+          oder
+          <a href="">Registrieren</a>
+        </div>
+    </form>
     </div>
-  </div>
-  <div class="notifications">
-    notifications
-  </div>
-  <div class="right_sidebar_nav">
-    navigation
-  </div>
-  <div class="blog_posts">
-    blog posts
+    <div id="right_sidebar_notifications">
+      notifications
+    </div>
+    <div id="right_sidebar_navigation">
+      <a href=""><i class="fa fa-bug fa-fw navigation_symbols" aria-hidden="true"></i> Development Log</a><br />
+      <a href=""><i class="fa fa-life-ring fa-fw navigation_symbols" aria-hidden="true"></i> Support-Center</a><br />
+      <a href=""><i class="fa fa-question fa-fw navigation_symbols" aria-hidden="true"></i> Häufig gestellte Fragen</a>
+    </div>
+    <div id="blog_posts">
+      blog posts
+    </div>
   </div>
 </div>
