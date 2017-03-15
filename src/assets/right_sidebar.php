6180f94278a80e581769f5f01f3dae70f90ee429
RubenWinkler <r.winkler1412@gmail.com>
2017-03-15 22:09:06 +0100

    Weiter gemacht!

--- src/assets/right_sidebar.php
+++ src/assets/right_sidebar.php
@@ -1,14 +1,6 @@
 <div class="col-lg-2 col-md-4 col-sm-4 hidden-xs" id="right_sidebar_col">
   <div id="right_sidebar">
   <?php include("./../src/assets/login.php"); ?>
-    <div id="right_sidebar_nav">
-      <ul class="nav nav-pills nav-stacked">
-        <li role="presentation" class="active"><a href="#">Startseite</a></li>
-        <li role="presentation"><a href="#">YouTube-Blog</a></li>
-        <li class=""><a href="index.php">Mitglieder</a></li>
-        <li class=""><a href="index.php">Gruppen</a></li>
-        <li role="presentation"><a href="#"></a></li>
-      </ul>
-    </div>
+  <?php include("./../src/assets/social_nav.php"); ?>
   </div>
 </div>
