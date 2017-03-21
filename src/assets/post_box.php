567c451f189d432ffa1a5e43573c961b1f076eba
Ruben Winkler <r.winkler1412@gmail.com>
2017-03-21 20:34:34 +0100

    Weiter!

--- src/assets/post_box.php
+++ src/assets/post_box.php
@@ -1,38 +1,24 @@
-<div id="post_box">
-  <div class="container-fluid">
+<div class="container-fluid" id="post_box">
+  <form>
     <div class="row">
-      <div class="col-md-1 hidden-sm hidden-xs">
-        <img class="" src="./../src/img/Profilbild.png" id="profilbild_post_box">
+      <div class="col-sm-12 hidden-xs">
+        <div id="post_box_avatar_cell">
+          <img src="./../src/img/Profilbild.png" id="post_box_avatar">
+        </div>
+        <span id="whats_new_text_span"><h4 id="whats_new_text">Was gibt's Neues RUBEN?</h4></span>
+        <textarea class="form-control" rows="1" name="post_textarea" id="postbox_textarea"></textarea><br />
       </div>
-      <div class="col-md-11 col-sm-12 col-xs-12" id="was_gibts_neues_und_text_box_rahmen">
-        <div class="row">
-          <div class="col-xs-12">
-            <h4>Was gibt's Neues RUBEN?</h4>
-          </div>
-        </div>
-        <form>
-          <div class="row">
-            <div class="col-xs-12">
-                <textarea class="form-control" rows="1" name="post_textarea"></textarea><br />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-12">
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-10">
-            </div>
-            <div class="col-xs-2">
-              <button class="btn btn-default" id="post_box_posten_button" type="submit">Posten</button>
-            </div>
-          </div>
-        </form>
-        <div class="row">
-          <div class="col-xs-12">
-          </div>
-        </div>
+      <div class="hidden-lg hidden-md hidden-sm col-xs-12">
+        <span id="whats_new_text_span_mobile"><h4 id="whats_new_text_mobile">Was gibt's Neues RUBEN?</h4></span>
+        <textarea class="form-control" rows="1" name="post_textarea" id="postbox_textarea_mobile"></textarea><br />
       </div>
     </div>
-  </div>
+    <div class="row">
+      <div class="col-xs-10">
+      </div>
+      <div class="col-xs-2">
+        <button class="btn btn-default" id="post_box_posten_button" type="submit">Posten</button>
+      </div>
+    </div>
+  </form>
 </div>
