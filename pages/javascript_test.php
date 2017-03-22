220efed51cbca965d94bf45cbc11be8ef052d05d
RubenWinkler <r.winkler1412@gmail.com>
2017-03-22 22:54:01 +0100

    JavaScript für Postbox gemacht! =)
    Oh boy, oh boy, oh boy... it works! *POGCHAMP*

---
+++ pages/javascript_test.php
@@ -0,0 +1,15 @@
+    <?php include("./../src/assets/head.php"); ?>
+
+
+    <script>
+    function test (a) {
+      if (a == "Video") {
+        document.write ("Video aufgerufen!");
+      }
+    }
+    </script>
+
+    <button type="button" onclick="test('Video');" value="Video">Video</button>
+
+
+    <?php include("./../src/assets/footer.php"); ?>
