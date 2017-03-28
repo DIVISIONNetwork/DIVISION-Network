604276cba61bc1519885821a012533e3839e40be
RubenWinkler <r.winkler1412@gmail.com>
2017-03-28 22:36:07 +0200

    Chat implementiert!
    POGCHAMP

--- src/assets/content.php
+++ src/assets/content.php
@@ -2,5 +2,5 @@
     <?php include("./../src/assets/left_sidebar.php"); ?>
     <?php include("./../src/assets/chronik.php"); ?>
     <?php include("./../src/assets/right_sidebar.php"); ?>
-    <?php include("./../src/assets/member_list.php"); ?>
+    <?php include("./../src/assets/chat_sidebar.php"); ?>
 </div>
