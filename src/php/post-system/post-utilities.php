df4baa8ea57c4e324e9562669ff10d503e641624
Ruben Winkler <r.winkler1412@gmail.com>
2017-05-02 12:28:03 +0200

    Weiter!

--- src/php/post-system/post-utilities.php
+++ src/php/post-system/post-utilities.php
@@ -1,7 +1,11 @@
 <?php
 
-function generatePosts() {
-
+function convertYTLinkToEmbed($youtube_link) {
+	return preg_replace(
+		"/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
+		"www.youtube.com/embed/$2",
+    $youtube_link
+	);
 }
 
  ?>
